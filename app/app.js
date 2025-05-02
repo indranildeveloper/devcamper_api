b6d7c3c0bfa1604d7df6617ae9be0ffac3042b7b
Indranil Halder <indranilhalder.dev@gmail.com>
2025-05-02 14:45:25 +0530

    Mongodb database connection

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ import { fileURLToPath } from "url";
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import colors from "colors";
+import connectDB from "../database/database.js";
 import bootcampRoutes from "../routes/bootcampRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +12,9 @@ const __dirname = path.dirname(__filename);
 
 // Load env vars
 dotenv.config({ path: path.join(__dirname, "../config/config.env") });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -23,8 +28,16 @@ app.use("/api/v1/bootcamps", bootcampRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow
+      .bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
 });
