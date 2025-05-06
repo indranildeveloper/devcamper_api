cf86454bf9f3d980ae4dadf638e10e7870c00196
Indranil Halder <indranilhalder.dev@gmail.com>
2025-05-06 20:30:56 +0530

    Average cost calculation by using aggregation pipeline
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- models/CourseModel.js
+++ models/CourseModel.js
@@ -38,6 +38,41 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+// Static method to get average of course tuition
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log("Calculating average cost".blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Call getAverage cost after save
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverage cost before remove
+CourseSchema.pre("deleteOne", { document: true, query: false }, function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 const Course = mongoose.model("Course", CourseSchema);
 
 export default Course;
